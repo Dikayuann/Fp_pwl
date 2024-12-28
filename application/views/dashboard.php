b4c40f33e23c8c6c8e5cadd040c5d16d187f547a
janucesaratmaja <janucesar@students.amikom.ac.id>
2024-12-28 23:27:24 +0700

    update Ui sesuai figma

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .sidebar {
-            background-color: #f8f9fa;
+            background-color: #D9D9D9;
             height: 100vh;
             padding-top: 20px;
         }
--- application/views/login.php
+++ application/views/login.php
@@ -5,13 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Bimbelindo</title>
     <style>
-        
-        body 
-        
-        {
+        body {
             font-family: Arial, sans-serif;
             margin: 0;
-            padding: 0;
+            padding: ;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -19,50 +16,62 @@
             background: url('background.jpg') no-repeat center center;
             background-size: cover;
         }
+
         .login-container {
             background-color: rgba(255, 255, 255, 0.9);
-            padding: 20px;
-            border-radius: 10px;
+            padding: 40px;
+            border-radius: 15px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
             width: 100%;
             max-width: 400px;
             text-align: center;
         }
+
         .login-container img {
             width: 80px;
             margin-bottom: 10px;
+            
         }
+
         .login-container h1 {
             font-size: 24px;
-            margin: 10px 0;
-            color: #333;
+            margin: 30px 0;
+            color: #2b5c87;
+            margin-bottom: 90px;
         }
+
         .login-container p {
-            margin: 0 0 20px;
+            margin: 0 0 60px;
             color: #666;
+            margin-bottom: 30px;
         }
+
         .login-container input {
-            width: 100%;
+            width: 95%;
             padding: 10px;
-            margin-bottom: 10px;
+            margin-bottom: 20px;
             border: 1px solid #ccc;
             border-radius: 5px;
             font-size: 16px;
+            
         }
+
         .login-container button {
             width: 100%;
             padding: 10px;
-            background-color: #007bff;
+            background-color: #2b5c87;
             border: none;
             border-radius: 5px;
             color: white;
             font-size: 16px;
             cursor: pointer;
+            margin-bottom: 20px;
         }
+
         .login-container button:hover {
-            background-color: #0056b3;
+            background-color: #4a7db1;
         }
-        
+
     </style>
 </head>
 <body>
